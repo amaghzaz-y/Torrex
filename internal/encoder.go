ea80b3c071f19365ae5c09448e6f605d0acbeb9e
Yassine AMAGHZAZ <yassine.amaghzaz@outlook.com>
2023-08-23 12:29:51 +0200

    Add Video Transcoder to HLS using ffmpeg

--- internal/encoder.go
+++ internal/encoder.go
@@ -1,2 +1,7 @@
 package torrex
 
+import ffmpeg "github.com/u2takey/ffmpeg-go"
+
+func TranscodeVideoToHLS(input string, output string) error {
+	return ffmpeg.Input(input).Output(output).OverWriteOutput().Run()
+}
--- main.go
+++ main.go
@@ -1,26 +1,25 @@
 package main
 
-import (
-	"log"
-	"time"
-
-	torrex "github.com/amaghzaz-y/torrex/internal"
-)
+import torrex "github.com/amaghzaz-y/torrex/internal"
 
 func main() {
-	c := torrex.NewClient()
-	if err := c.RegisterTorrentFile("assets/looney.torrent"); err != nil {
-		log.Fatalln(err)
+	err := torrex.TranscodeVideoToHLS("Looney/Looney.mkv", "Looney/hls/Looney.m3u8")
+	if err != nil {
+		panic(err)
 	}
-	c.DownloadFiles()
-	for {
-		if c.IsDownloadComplete() {
-			break
-		}
-		c.LogInfo()
-		time.Sleep(5 * time.Second)
-	}
-	log.Println("finished download")
+	// c := torrex.NewClient()
+	// if err := c.RegisterTorrentFile("assets/looney.torrent"); err != nil {
+	// 	log.Fatalln(err)
+	// }
+	// c.DownloadFiles()
+	// for {
+	// 	if c.IsDownloadComplete() {
+	// 		break
+	// 	}
+	// 	c.LogInfo()
+	// 	time.Sleep(5 * time.Second)
+	// }
+	// log.Println("finished download")
 	// link, err := scraper.FetchMovieLink("spider man")
 	// if err != nil {
 	// 	log.Fatal(err)
